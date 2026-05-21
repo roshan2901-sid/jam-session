982470d6c03232806bc667db3fa25c9534a7ecdb
Roshan Siddarth <g.roshansiddarth9c.takshasila@gmail.com>
2026-05-21 20:25:12 +0530

    Fixed App routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import Tickets from "./pages/Tickets"
 import Summary from "./pages/Summary"
 import Success from "./pages/Success"
-import Admin from "./pages/Admin"
+import AdminScan from "./pages/AdminScan"
 
 function App() {
+
   return (
     <BrowserRouter>
 
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
 
-        <Route path="/tickets" element={<Tickets />} />
+        <Route
+          path="/tickets"
+          element={<Tickets />}
+        />
 
-        <Route path="/summary" element={<Summary />} />
+        <Route
+          path="/summary"
+          element={<Summary />}
+        />
 
-        <Route path="/success" element={<Success />} />
+        <Route
+          path="/success"
+          element={<Success />}
+        />
 
-        <Route path="/admin" element={<Admin />} />
+        <Route
+          path="/admin"
+          element={<AdminScan />}
+        />
 
       </Routes>
 
