9353b0794acfdc4d76f0da21a173f2657a8e651a
Roshan Siddarth <g.roshansiddarth9c.takshasila@gmail.com>
2026-05-25 23:42:52 +0530

    Fixed imports correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import Home from "./pages/Home"
-import Tickets from "./pages/Tickets"
-import Summary from "./pages/Summary"
-import Success from "./pages/Success"
-import AdminScan from "./pages/AdminScan"
+import Home from "./pages/Home.jsx"
+import Tickets from "./pages/Tickets.jsx"
+import Summary from "./pages/Summary.jsx"
+import Success from "./pages/Success.jsx"
+import AdminScan from "./pages/AdminScan.jsx"
 
 function App() {
 
@@ -14,30 +14,11 @@ function App() {
 
       <Routes>
 
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/tickets"
-          element={<Tickets />}
-        />
-
-        <Route
-          path="/summary"
-          element={<Summary />}
-        />
-
-        <Route
-          path="/success"
-          element={<Success />}
-        />
-
-        <Route
-          path="/admin"
-          element={<AdminScan />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/admin" element={<AdminScan />} />
 
       </Routes>
 
