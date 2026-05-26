259fe62d1feca60711f6414fefb9ad3835536055
Roshan Siddarth <g.roshansiddarth9c.takshasila@gmail.com>
2026-05-26 23:27:48 +0530

    fixed email testing

--- src/App.jsx
+++ src/App.jsx
@@ -17,24 +17,36 @@ function App() {
         headers: {
           "Content-Type": "application/json",
         },
+
         body: JSON.stringify({
-          email: "24r11a66c1@gcet.edu.in",
+          email: "g.roshansiddarth9c.takshasila@gmail.com",
           name: "Roshan",
           ticketId: "OYM1029",
         }),
+
       });
 
       const data = await response.json();
 
       console.log(data);
 
-      alert("Email Sent Successfully!");
+      if (response.ok) {
+
+        alert("Email Sent Successfully!");
+
+      } else {
+
+        alert("Email Failed");
+
+        console.log(data);
+
+      }
 
     } catch (error) {
 
       console.error(error);
 
-      alert("Failed to send email");
+      alert("Something went wrong");
 
     }
 
@@ -52,13 +64,14 @@ function App() {
             position: "fixed",
             top: "20px",
             right: "20px",
-            zIndex: 9999,
+            zIndex: "9999",
             padding: "12px 20px",
             background: "red",
             color: "white",
             border: "none",
             borderRadius: "10px",
             cursor: "pointer",
+            fontWeight: "bold",
           }}
         >
           Send Test Email
